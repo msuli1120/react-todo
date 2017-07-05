97434b54bd1d930200145b7f574a709d8f5a90ec
Xing Li <msuli_1120@ymail.com>
2017-07-05 13:54:34 -0700

    add remove function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var todos = [
+  {
+   todoTitle: 'My first todo',
+    todoResponsible: 'Xing',
+    todoDescription: 'My first todo description',
+    todoPriority: 'low'
+  },
+  {
+    todoTitle: 'My second todo',
+    todoResponsible: 'Xing',
+    todoDescription: 'My second todo description',
+    todoPriority: 'hign'
+  },
+  {
+    todoTitle: 'My third todo',
+    todoResponsible: 'Xing',
+    todoDescription: 'My third todo description',
+    todoPriority: 'medium'
+  },
+  {
+    todoTitle: 'My forth todo',
+    todoResponsible: 'Xing',
+    todoDescription: 'My forth todo description',
+    todoPriority: 'high'
+  },
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos
+    };
+  }
+
+  handleRemoveTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter(function(e, i){
+        return i !== index;
+      })
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h4>Todo Count: <span className="badge">{this.state.todos.length}</span></h4>
+        <ul className="list-group">
+          {this.state.todos.map((todo, index) =>
+            <li className="list-group-item" key={index}>
+              <h4 className="list-group-item-heading">{todo.todoTitle} <small><span className="label label-info">{todo.todoPriority}</span></small></h4>
+              <p><span className="glyphicon glyphicon-user"></span> {todo.todoResponsible}</p>
+              <p>{todo.todoDescription}</p>
+              <button className="btn btn-danger btn-sm" onClick={this.handleRemoveTodo.bind(this, index)}><span className="glyphicon glyphicon-trash"></span></button>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
