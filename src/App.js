e0a741bad712a4afb8e69c8e94736513597d5111
Xing Li <msuli_1120@ymail.com>
2017-07-05 15:06:38 -0700

    add add todo function

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
     this.state = {
       todos
     };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
   handleRemoveTodo(index) {
@@ -45,9 +46,16 @@ class App extends Component {
     });
   }
 
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    });
+  }
+
   render() {
     return (
       <div className="container">
+        <TodoInput onAddTodo={this.handleAddTodo} />
         <h4>Todo Count: <span className="badge">{this.state.todos.length}</span></h4>
         <ul className="list-group">
           {this.state.todos.map((todo, index) =>
@@ -62,6 +70,92 @@ class App extends Component {
       </div>
     );
   }
+}
+
+class TodoInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoTitle: '',
+      todoResponsible: '',
+      todoDescription: '',
+      todoPriority: 'Lowest',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onAddTodo(this.state);
+    this.setState({
+      todoTitle: '',
+      todoResponsible: '',
+      todoPriority: 'Lowest',
+      todoDescription: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Add New Todo</h4>
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="inputTodoTitle" className="col-sm-2 control-label">Todo</label>
+            <div className="col-sm-10">
+              <input name="todoTitle" type="text" className="form-control" id="inputTodoTitle" value={this.state.todoTitle} onChange={this.handleInputChange} placeholder="Title"></input>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputTodoResponsible" className="col-sm-2 control-label">Responsible</label>
+            <div className="col-sm-10">
+              <input name="todoResponsible" type="text" className="form-control" id="inputTodoResponsible" value={this.state.todoResponsible} onChange={this.handleInputChange} placeholder="Responsible"></input>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputTodoDesc" className="col-sm-2 control-label">Description</label>
+            <div className="col-sm-10">
+              <textarea name="todoDescription" className="form-control" rows="3" id="todoDescription" value={this.state.todoDescription} onChange={this.handleInputChange}></textarea>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputTodoPriority" className="col-sm-2 control-label">Priority</label>
+            <div className="col-sm-10">
+              <select name="todoPriority" id="todoPriority" className="form-control" value={this.state.todoPriority} onChange={this.handleInputChange}>
+                <option>Lowest</option>
+                <option>Low</option>
+                <option>Medium</option>
+                <option>High</option>
+                <option>Highest</ option>
+              </select>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-success">Add Todo</button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
